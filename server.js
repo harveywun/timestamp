cdab884f7439fcd34386df978cec58a6f8685f0b
HWUN <harvey.wun@gmail.com>
2017-03-05 05:03:23 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var express = require('express')
+var express = require('express');
+var app = express();
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var d;
 
-var app = express()
+app.get('/:date', function (req, res) {
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+    if(isNaN(req.params.date)){
+        var parts = req.params.date.match(/^([A-Za-z]+)\s(\d\d),\s(\d{4})$/);
+        d = new Date(parts[2] + " " + parts[1] + " " + parts[3]);
+    } else {
+        d = new Date(1000 * parseInt(req.params.date, 10));
+    }
+    
+    res.send({unix: d.getTime()/1000, natural: monthNames[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()});
+
 })
 
 app.listen(8080, function () {
